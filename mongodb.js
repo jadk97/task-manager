0b6d79d600f816012f5e1a6d16fff305173f980f
Jad K <jad_dis@hotmail.com>
2020-01-18 01:40:44 -0500

    adjusted the project's folder structure and setup mongoose

--- mongodb.js
+++ mongodb.js
@@ -1,24 +1,11 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-  if(error){
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
     return console.log("Unable to connect to database");
   }
 
-  // console.log("Successfully connected!");
-  const db = client.db(databaseName);
-  db.collection("users").insertOne({
-    name: "JK",
-    age: 22
-  }, (error, result) => {
-    if(error){
-      return console.log("Unable to insert user");
-    }
-
-    console.log(result.ops);
-  })
 });
