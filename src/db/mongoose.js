682fd8749b9317cbdb491efb610482f9cd791fcc
Jad K <jad_dis@hotmail.com>
2020-01-18 02:47:49 -0500

    added validation checks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,12 +8,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    }
   }
 });
+
 
 const Task = mongoose.model('Task', {
   description: {
@@ -23,3 +44,14 @@ const Task = mongoose.model('Task', {
   }
 });
 
+const me = new User({
+  name: " JK   ",
+  email: "  jk@email.com   "
+})
+
+me.save().then((result) => {
+  console.log(result);
+})
+.catch((error) => {
+  console.log(error);
+})
