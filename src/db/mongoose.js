2e483e16cf12609d76c31befd387fb9fe6a66e67
Jad K <jad_dis@hotmail.com>
2020-01-18 04:14:41 -0500

    restructured the file/folder layout and setup a user creation endpoint with express

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,48 +1,9 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
-});
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value){
-      if(value.toLowerCase().includes("password")){
-        throw new Error("Password cannot contain the word 'password' in it.")
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    }
-  }
 });
 
 
@@ -58,23 +19,3 @@ const Task = mongoose.model('Task', {
   }
 });
 
-// const me = new User({
-//   name: " JK   ",
-//   email: "  jk@email.com   ",
-//   password: "password"
-// })
-
-// const task = new Task({
-//   description: "Wash the dishes"
-// })
-
-// task.save().then((result) => {
-//   console.log(result);
-// })
-// .catch((error) => {console.log(error)});
-// me.save().then((result) => {
-//   console.log(result);
-// })
-// .catch((error) => {
-//   console.log(error);
-// })
