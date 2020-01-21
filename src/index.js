dd17bfd375990c1073afb3b7c64ac21412e63600
Jad K <jad_dis@hotmail.com>
2020-01-21 03:58:16 -0500

    setup middleware to further implement jwt authentication and tweaked the users GET route to make use of it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 require("./db/mongoose");
 
-const User = require("./models/user");
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use((req, res, next) => {
+   
+
+// })
 
 app.use(express.json());
 app.use(userRouter);
