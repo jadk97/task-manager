8461cda3d7536f670570cb5b8b7e1a3674a8b535
Jad K <jad_dis@hotmail.com>
2020-01-21 01:08:57 -0500

    added delete route for tasks endpoint

--- src/index.js
+++ src/index.js
@@ -146,6 +146,19 @@ app.patch(`/tasks/:id`, async (req, res) => {
     res.status(400).send();
   }
 })
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task){
+      res.status(404).send()
+    }
+    res.send(task);
+  }
+  catch(error){
+    res.status(500).send();
+  }
+})
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
