35f54777594f31d900b015e0aa8e2a4cfcfd964e
Jad K <jad_dis@hotmail.com>
2020-01-21 02:37:46 -0500

    added a login route for users and slightly modified the patch route for tasks

--- src/models/user.js
+++ src/models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -40,8 +41,23 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-userSchema.pre('save', async function (next) {
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
 
+  if(!user){
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if(!isMatch){
+    throw new Error("Unable to login");
+  }
+
+  return user;
+}
+
+userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 8);
   }
